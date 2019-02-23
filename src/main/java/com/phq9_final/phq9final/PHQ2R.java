453e08c97887b9deb56f6cddec4c01fac0e03174
Patient1 <>
2019-02-23 17:42:23 +0800

    {/src/main/java/com/phq9_final/phq9final/PHQ2R.java}

--- src/main/java/com/phq9_final/phq9final/PHQ2R.java
+++ src/main/java/com/phq9_final/phq9final/PHQ2R.java
@@ -6,12 +6,23 @@ package com.phq9_final.phq9final;
 
 public class PHQ2R implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public PHQ2R() {
-    }
+	private java.lang.Boolean PHQ2Result;
 
+	public PHQ2R() {
+	}
 
+	public java.lang.Boolean getPHQ2Result() {
+		return this.PHQ2Result;
+	}
 
+	public void setPHQ2Result(java.lang.Boolean PHQ2Result) {
+		this.PHQ2Result = PHQ2Result;
+	}
+
+	public PHQ2R(java.lang.Boolean PHQ2Result) {
+		this.PHQ2Result = PHQ2Result;
+	}
 
 }
