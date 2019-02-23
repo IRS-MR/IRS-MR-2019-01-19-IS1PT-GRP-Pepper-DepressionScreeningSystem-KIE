b307fcd575e87c491f43e11915d048b26002c77d
Patient1 <>
2019-02-23 18:09:27 +0800

    {/src/main/java/com/phq9_final/phq9final/Treatment3.java}

--- src/main/java/com/phq9_final/phq9final/Treatment3.java
+++ src/main/java/com/phq9_final/phq9final/Treatment3.java
@@ -6,12 +6,23 @@ package com.phq9_final.phq9final;
 
 public class Treatment3 implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Treatment3() {
-    }
+	private java.lang.Boolean treat3;
 
+	public Treatment3() {
+	}
 
+	public java.lang.Boolean getTreat3() {
+		return this.treat3;
+	}
 
+	public void setTreat3(java.lang.Boolean treat3) {
+		this.treat3 = treat3;
+	}
+
+	public Treatment3(java.lang.Boolean treat3) {
+		this.treat3 = treat3;
+	}
 
 }
